1112678ea9fc9dbd49faad2fb05eb44fa7bfbbfc
Marta_BM <marta@commite.co>
2019-02-27 07:35:45 +0100

    Registro y autenticación

--- base/views.py
+++ base/views.py
@@ -27,9 +27,23 @@ def sign_up(request):
                                         )
                 user = authenticate(email=email, password=password)
                 login(request, user)
-                return HttpResponseRedirect('/')
+                return HttpResponseRedirect('/user/user_page')
             else:
                 raise forms.ValidationError('This email already exists')
     else:
         form = SignUpForm()
     return render(request, 'base/sign_up.html', {'form': form})
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(email=email, password=password)
+        login(request, user)
+        return HttpResponseRedirect('/user/user_page')
+    return render(request, 'base/sign_in.html', {})
+
+
+def user_page(request):
+    return render(request, 'base/user_page.html', {})
--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,6 @@ from base import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('user/new', views.sign_up, name='sign_up'),
+    path('user/login', views.sign_in, name="sign_in"),
+    path('user/user_page', views.user_page, name="user_page"),
     ]
